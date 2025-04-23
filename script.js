235654c5c7dc5d81719755017bca7b47f9475ca9
carellice <flavio.cecca00@gmail.com>
2025-04-23 14:01:21 +0200

    FIX export datas filename

--- script.js
+++ script.js
@@ -1457,9 +1457,14 @@ function formatCurrency(amount) {
     }).format(amount);
 }
 
+// function formatDateForFilename() {
+//     const now = new Date();
+//     return `${now.getFullYear()}${(now.getMonth() + 1).toString().padStart(2, '0')}${now.getDate().toString().padStart(2, '0')}_${now.getHours().toString().padStart(2, '0')}${now.getMinutes().toString().padStart(2, '0')}`;
+// }
+
 function formatDateForFilename() {
     const now = new Date();
-    return `${now.getFullYear()}${(now.getMonth() + 1).toString().padStart(2, '0')}${now.getDate().toString().padStart(2, '0')}_${now.getHours().toString().padStart(2, '0')}${now.getMinutes().toString().padStart(2, '0')}`;
+    return `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}`;
 }
 
 function getContrastColor(hexColor) {
