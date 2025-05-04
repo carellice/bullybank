5d9a9cb79c2c701537407f54a089d706543c862e
carellice <flavio.cecca00@gmail.com>
2025-05-04 16:34:02 +0200

    FIX PWA

--- service-worker.js
+++ service-worker.js
@@ -1,74 +1,27 @@
-const CACHE_NAME = 'bullybank-cache-v9';
-const urlsToCache = [
-    './',
-    './index.html',
-    './style.css',
-    './script.js',
-    './icons/icon-72x72.png',
-    './icons/icon-96x96.png',
-    './icons/icon-128x128.png',
-    './icons/icon-144x144.png',
-    './icons/icon-152x152.png',
-    './icons/icon-192x192.png',
-    './icons/icon-384x384.png',
-    './icons/icon-512x512.png'
-];
+// Non definiamo più il CACHE_NAME o urlsToCache
 
-// Installazione del Service Worker
+// Installazione del Service Worker - non fa nulla di particolare
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Cache aperta');
-                return cache.addAll(urlsToCache);
-            })
-    );
+    console.log('Service Worker installato, ma senza cache');
+    self.skipWaiting(); // Attivazione immediata
 });
 
-// Attivazione del Service Worker
+// Attivazione del Service Worker - elimina tutte le cache esistenti
 self.addEventListener('activate', event => {
-    const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
+                    console.log('Eliminazione cache:', cacheName);
+                    return caches.delete(cacheName);
                 })
             );
         })
     );
 });
 
-// Gestione delle richieste di rete
+// Gestione delle richieste di rete - sempre dalla rete, mai dalla cache
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                // Ritorna la risposta dalla cache, se presente
-                if (response) {
-                    return response;
-                }
-
-                // Altrimenti, recupera dalla rete
-                return fetch(event.request)
-                    .then(response => {
-                        // Controlla se la risposta è valida
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-
-                        // Clona la risposta per poterla usare sia per la cache che per il browser
-                        const responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    });
-            })
-    );
+    // Passthrough diretto alla rete senza caching
+    // Non facciamo nulla di speciale, lasciamo che il browser gestisca normalmente la richiesta
 });
